d52e59646e65e029b3885458e95e281df90157e1
zhidong.sun <zhidong.sun@shmxrd.com>
2023-08-23 10:34:07 +0800

    upyuns3client

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='upyuns3client',
+    version='0.1.0',
+    url='https://github.com/evansuner/upyuns3client',
+    author='Evan',
+    author_email='author@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),
+    install_requires=[
+        'boto3==1.28.30',
+    ],
+)
