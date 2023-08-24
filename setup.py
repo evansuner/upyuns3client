8e91d58ded80ca47a5f764ff7525f23217560b70
zhidong.sun <zhidong.sun@shmxrd.com>
2023-08-24 16:53:46 +0800

    deleted:    .travis.yml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="upyuns3client",
-    version="0.1.0",
+    version="0.1.2",
     url="https://github.com/evansuner/upyuns3client",
     author="Evan",
     author_email="zhidong.s@outlook.com",
