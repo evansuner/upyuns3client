61c6d14c2c60e6345a3ada556807c30317a5bfa5
zhidong.sun <zhidong.sun@shmxrd.com>
2023-08-24 17:54:23 +0800

    commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="upyuns3client",
-    version="0.1.2",
+    version="0.1.3",
     url="https://github.com/evansuner/upyuns3client",
     author="Evan",
     author_email="zhidong.s@outlook.com",
